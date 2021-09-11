e92cfbda0f9a6c98fffa33dcc4783b678b91b817
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2021-09-11 16:22:14 -0400

    Fix

--- public/php/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$body = file_get_contents('php://input');
-$body = urldecode($body);
-$body = str_replace("saveString=", "", $body);
-$body = str_replace("&", "\n", $body);
-
-echo $body;
-?>
