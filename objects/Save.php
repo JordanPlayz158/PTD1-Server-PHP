25576dc2e9f828a687c6f74970cf59d599093a58
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2021-10-03 21:43:06 -0400

    Changed variable names to match standard variable naming conventions.

--- objects/Save.php
+++ objects/Save.php
@@ -1,8 +1,9 @@
 <?php
 class Save {
+    public $num;
     // called levelUnlocked (in SWF)
-    public $Advanced = 0;
-    public $Advanced_a = 0;
+    public $advanced = 0;
+    public $advanced_a = 0;
     // called HMP (in saveAccount Action)
     public $p_numPoke = 0;
     // called HMI (in saveAccount Action)
@@ -10,19 +11,19 @@ class Save {
     public $p_numItem = 0;
     // used for hacker check, number of shiny pokemon you have (NOT SHADOW)
     public $p_hs = 0;
-    public $Nickname = "NA";
-    public $Badges = 0;
-    public $avatar = "none";
+    public $nickname = 'N/A';
+    public $badges = 0;
+    public $avatar = 'none';
     // called haveFlash (in SWF), assuming it's talking about the Flash TM
-    public $Classic = 0;
-    // split by "|" and called extraInfo
-    public $Classic_a = "";
+    public $classic = 0;
+    // split by '|' and called extraInfo
+    public $classic_a = '';
     // called clevelCompleted (in SWF)
-    public $Challenge = 0;
-    public $Money = 50;
-    public $NPCTrade = 0;
-    public $ShinyHunt = 0;
-    public $Version = 2;
-    public $poke = array();
+    public $challenge = 0;
+    public $money = 50;
+    public $npcTrade = 0;
+    public $shinyHunt = 0;
+    public $version = 2;
+    public $pokes = array();
     public $items = array();
 }
