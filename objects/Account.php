4ecae09aa8620a0f8648ba3de8e4949d974f327f
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2021-10-03 21:37:43 -0400

    Account object revised, removed unneccessary variables, renamed variables to better reflect proper variable naming conventions, removed certain default values.

--- objects/Account.php
+++ objects/Account.php
@@ -1,24 +1,18 @@
 <?php
-
 class Account {
     // Never sent in response
-    public $Email;
+    public $email;
     // Never sent in response
-    public $Pass;
-    // ONLY 1 (not per save)
-    public $CurrentSave = 10000000000000;
+    public $pass;
     // myTID is TrainerID (saveAccount Action)
     // ONLY 1 (not per save)
-    public $TrainerID;
-    // myVID is ProfileID (saveAccount Action)
-    // ONLY 1 (not per save)
-    public $ProfileID;
+    public $trainerId;
 
-    public $save = array();
+    public $saves = array();
 
-    public $accNickname = "NA";
-    public $dex1 = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-    public $dex1Shiny = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-    public $dex1Shadow = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+    public $accNickname = 'N/A';
+    public $dex1;
+    public $dex1Shiny;
+    public $dex1Shadow;
 }
 ?>
