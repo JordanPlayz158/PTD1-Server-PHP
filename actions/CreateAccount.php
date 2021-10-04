e7fecab96bfed7418b228b9a2a39d0fc1caa3d4a
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2021-10-03 21:40:05 -0400

    Revised CreateAccount as checking if the account exists is now newPoke8.php's responsbility, changed code due to MySQL change.

--- actions/CreateAccount.php
+++ actions/CreateAccount.php
@@ -2,33 +2,24 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../Utils.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../objects/Account.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../objects/Save.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/php/newPoke8.php');
 
 class CreateAccount {
-    function __construct($accounts, $post_data) {
-        if(Utils::getAccount($accounts, $post_data) != null) {
-            Utils::response("Result", "Failure");
-            Utils::response("Reason", "taken");
-
-            echo Utils::getResponse();
-            return;
-        }
-
+    function __construct($post_data) {
         $account = new Account();
-        $account -> Email = $post_data['Email'];
-        $account -> Pass = $post_data['Pass'];
-        $account -> TrainerID = $trainerID = Utils::generateValidTrainerID($accounts);
-        $account -> ProfileID = Utils::generateValidProfileID(10000000000000, $trainerID);
+        $account -> email = $post_data['Email'];
+        $account -> pass = $post_data['Pass'];
+        
+        $trainerIDs = Utils::$mysql->conn->query('SELECT trainerId FROM accounts');
+        $trainerIDs = $trainerIDs -> fetch_all();
+        
+        $account -> trainerId = Utils::generateValidTrainerID($trainerIDs);
 
         for($i = 1; $i <= 3; $i++) {
-            $account -> save[] = new Save();
+            $account -> saves[] = new Save();
         }
 
-        $accounts[] = $account;
-        newPoke8::$accounts = $accounts;
-
-        Utils::saveData($accounts);
-        new LoadAccount(newPoke8::$accounts, $post_data);
+        Utils::$mysql->createAccount($account);
+        new LoadAccount($account);
     }
 }
 ?>
