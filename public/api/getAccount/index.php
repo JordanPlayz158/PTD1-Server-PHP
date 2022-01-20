e132a4a217dc1fe79991b21b43276426bde49c9f
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-01-19 20:13:31 -0500

    - Added dependencies to composer for documentation to appear - Apparently there is no previous version of account.php so it'll be included in this commit - Updated crossdomain.xml in an attempt to get https communication with SWF to work (failed so far) - Updated Dependencies to account for new dependencies required - Moved getAccount endpoint from /api/admin to just /api as it makes more sense as it's a public api used by normal users

---
+++ public/api/getAccount/index.php
@@ -0,0 +1,52 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../MySQL.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../RedisCache.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../Utils.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../objects/Account.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../objects/Save.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/../objects/Poke.php');
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $config = require($_SERVER['DOCUMENT_ROOT'] . '/../config.php');
+
+    $mysql = new MySQL($config);
+    $conn = $mysql->conn;
+
+    $redis = new RedisCache($config);
+
+    session_start();
+    $email = $redis->conn->get($_SESSION['account_token']);
+
+    $accounts = getAccountDataByEmail($conn, 'accounts', $email);
+
+    if($accounts === null) {
+        echo json_encode(array('error' => 'No account found'));
+    } else if(count($accounts) === 1) {
+        $account = new Account();
+        $account->parse($accounts[0]);
+        unset($account->pass);
+
+        $saves = getAccountDataByEmail($conn, 'saves', $email);
+
+        foreach ($saves as $saveArray) {
+            $save = new Save();
+            $save->parse($saveArray);
+
+            $account->saves[] = $save;
+        }
+
+        $pokes = getAccountDataByEmail($conn, 'pokes', $email);
+
+        // Pokemon
+        foreach ($pokes as $pokeArray) {
+            $poke = new Poke();
+            $poke->parse($pokeArray);
+
+            $account->saves[$pokeArray['num']]->pokes[] = $poke;
+        }
+
+        echo json_encode($account);
+    }
+} else {
+    echo "Invalid Request Method! Accepted Method(s): GET";
+}
