57dd1f321158ec164e297103f9524cf9d280129a
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-10-12 13:16:15 -0400

    Updated achievements table layout to identify user by user id with foreign key constraint/reference rather than email and gave each achievement its own id.

---
+++ database/migrations/2022_10_04_202554_drop_email_from_achievements_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropColumns('achievements', 'email');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('achievements', function (Blueprint $table) {
+            $table->string('email', 50)->after('user_id');
+        });
+    }
+};
