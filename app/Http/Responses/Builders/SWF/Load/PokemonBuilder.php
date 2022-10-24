ea7c3dc05c0ac452c2c7cf6b422b3670c8af5629
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-10-24 15:46:57 -0400

    Forgot to include `return $this` for the builder to function properly and added logic to format variables correctly

--- app/Http/Responses/Builders/SWF/Load/PokemonBuilder.php
+++ app/Http/Responses/Builders/SWF/Load/PokemonBuilder.php
@@ -23,10 +23,11 @@ class PokemonBuilder
     protected int $m4;
     protected int $mSel;
 
-    public static function new($saveNum): self
+    public static function new($saveNum, $pokeNum): self
     {
         $new = new self();
         $new->saveNum = $saveNum;
+        $new->pokeNum = $pokeNum;
 
         return $new;
     }
@@ -34,121 +35,144 @@ class PokemonBuilder
     /**
      * @param string $nickname
      */
-    public function setNickname(string $nickname): void
+    public function setNickname(string $nickname): self
     {
         $this->nickname = $nickname;
+        return $this;
     }
 
     /**
      * @param int $num
      */
-    public function setNum(int $num): void
+    public function setNum(int $num): self
     {
         $this->num = $num;
+        return $this;
     }
 
     /**
      * @param int $lvl
      */
-    public function setLvl(int $lvl): void
+    public function setLvl(int $lvl): self
     {
         $this->lvl = $lvl;
+        return $this;
     }
 
     /**
      * @param int $exp
      */
-    public function setExp(int $exp): void
+    public function setExp(int $exp): self
     {
         $this->exp = $exp;
+        return $this;
     }
 
     /**
      * @param string $owner
      */
-    public function setOwner(string $owner): void
+    public function setOwner(string $owner): self
     {
         $this->owner = $owner;
+        return $this;
     }
 
     /**
      * @param string $targetType
      */
-    public function setTargetType(string $targetType): void
+    public function setTargetType(string $targetType): self
     {
         $this->targetType = $targetType;
+        return $this;
     }
 
     /**
      * @param string $tag
      */
-    public function setTag(string $tag): void
+    public function setTag(string $tag): self
     {
         $this->tag = $tag;
+        return $this;
     }
 
     /**
      * @param int $myID
      */
-    public function setMyID(int $myID): void
+    public function setMyID(int $myID): self
     {
         $this->myID = $myID;
+        return $this;
     }
 
     /**
      * @param int $pos
      */
-    public function setPos(int $pos): void
+    public function setPos(int $pos): self
     {
         $this->pos = $pos;
+        return $this;
     }
 
     /**
-     * @param int $noWay
+     * @param int $shiny
      */
-    public function setNoWay(int $noWay): void
+    public function getShiny(): int
     {
-        $this->noWay = $noWay;
+        return $this->noWay;
+    }
+
+    /**
+     * @param int $shiny
+     */
+    public function setShiny(int $shiny): self
+    {
+        $this->noWay = $shiny;
+        return $this;
     }
 
     /**
      * @param int $m1
      */
-    public function setM1(int $m1): void
+    public function setM1(int $m1): self
     {
         $this->m1 = $m1;
+        return $this;
     }
 
     /**
      * @param int $m2
      */
-    public function setM2(int $m2): void
+    public function setM2(int $m2): self
     {
         $this->m2 = $m2;
+        return $this;
     }
 
     /**
      * @param int $m3
      */
-    public function setM3(int $m3): void
+    public function setM3(int $m3): self
     {
         $this->m3 = $m3;
+        return $this;
     }
 
     /**
      * @param int $m4
      */
-    public function setM4(int $m4): void
+    public function setM4(int $m4): self
     {
         $this->m4 = $m4;
+        return $this;
     }
 
     /**
      * @param int $mSel
      */
-    public function setMSel(int $mSel): void
+    public function setMSel(int $mSel): self
     {
         $this->mSel = $mSel;
+        return $this;
     }
 
     /**
@@ -160,12 +184,20 @@ class PokemonBuilder
      */
     private function prepareVariableKeyNames(): void
     {
-        $pokePrefix = 'p' . $this->saveNum . '_poke_' . $this->num . '_';
+        $pokePrefix = 'p' . $this->saveNum . '_poke_' . $this->pokeNum . '_';
+
+        unset($this->saveNum);
+        unset($this->pokeNum);
 
         foreach (get_object_vars($this) as $key => $value) {
+            if($key === 'myID') {
+                $posKey = 'newPokePos_' . $this->pos;
+                $this->$posKey = $value;
+            }
+
             $newKey = $pokePrefix . $key;
             $this->$newKey = $value;
-            unset($key);
+            unset($this->$key);
         }
     }
 
