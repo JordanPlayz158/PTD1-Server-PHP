f27d6adf96e1b5760ad3d14743398182bd5ce8fa
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-11-03 02:13:34 -0400

    This adds the routes for the save grabbing and updating

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,7 @@ use App\Http\Controllers\Api\AccountController;
 use App\Http\Controllers\Api\SavesController;
 use App\Http\Controllers\Api\TokenController;
 use App\Http\Controllers\Api\TokensController;
-use App\Http\Controllers\Api\UserController;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,13 +26,18 @@ Route::middleware('auth:sanctum')->get('/tokens/{token}', [TokenController::clas
 
 Route::middleware('auth:sanctum')->delete('/tokens/{token}', [TokenController::class, 'remove']);
 
-Route::middleware('auth:sanctum')->get('/saves', [SavesController::class, 'get']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth:sanctum')->get('/account', [AccountController::class, 'get']);
 Route::middleware('auth:sanctum')->post('/account', [AccountController::class, 'post']);
+
+Route::middleware('auth:sanctum')->get('/saves', [SavesController::class, 'get']);
+// This seems unnecessary and more complex than the post/update with the num included
+//Route::middleware('auth:sanctum')->post('/saves', [SavesController::class, 'post']);
+
+Route::middleware('auth:sanctum')->get('/saves/{num}', [SavesController::class, 'getSpecificSave']);
+Route::middleware('auth:sanctum')->post('/saves/{num}', [SavesController::class, 'postSpecificSave']);
 
 require __DIR__.'/auth.php';
