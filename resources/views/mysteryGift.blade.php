e8211963bd2ca16f1f4f65539b6a718d4d5b2deb
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-11-03 23:12:10 -0400

    Added mysteryGift.blade.php so by extension, that covers `/p/mystery-gift.html`, `/games/ptd/dailyCode.php`, and `/games/ptd/dailyCode.html`

---
+++ resources/views/mysteryGift.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mystery Gift</title>
+    <meta charset="UTF-8">
+    <link rel='stylesheet' type='text/css' href='/_static/css/base.css'>
+    <link rel='stylesheet' type='text/css' href='/_static/css/suckerfish.css'>
+    <link rel='stylesheet' type='text/css' href="/_static/css/style.css">
+    <!--<script type='text/javascript' src='logging.js'></script>-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+    </script>
+    <script>
+        $(function () {
+            $('#header').load('/_static/html/header.html');
+            $('#nav').load('/_static/html/nav.html');
+        })
+    </script>
+</head>
+<body>
+<div id="header"></div>
+<div id="content">
+    <div id="nav"></div>
+    <table id="content_table">
+        <tbody>
+        <tr>
+            <td id="main">
+                <div class="block">
+                    <div class="title center">
+                        <p>Mystery Gift</p>
+                    </div>
+                    <div class="content center" id="contentDiv">
+                        <p>sndgames - Missingno.</p>
+                    </div>
+                </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
