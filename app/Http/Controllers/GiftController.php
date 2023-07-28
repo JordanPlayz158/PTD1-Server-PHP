ce474fc080b7e6fe47f511a1252d6b411711bcea
WahanH <Wahan2500@hotmail.com>
2023-07-28 02:16:56 +0200

    added the daily feature

--- app/Http/Controllers/GiftController.php
+++ app/Http/Controllers/GiftController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class GiftController extends Controller
 {
     public function getGift($button)
     {
+        
         $user = User::find(Auth::user()->id);
         $save = $user->selectedSave();
-        $randomNumber = mt_rand(0, 1);
+        $randomNumber = mt_rand(0, 1);          
+        $currentDate = Carbon::parse(Carbon::now())->format('Y-m-d');
+
+        if ($user->last_used_dg === $currentDate)
+        {
+            return redirect()->back();
+        }
 
         // Define the gift costs for each button
         $giftCosts = [
@@ -26,7 +34,6 @@ class GiftController extends Controller
             2 => [10000, 25000],
             3 => [50000, 100000],
         ];
-
         // Retrieve the user's / save's info
         $money = $save->money;
         $casinoCoins = $user->casino_coins;
@@ -46,8 +53,11 @@ class GiftController extends Controller
 
         // Update the user's casino coins after buying the daily gift
         $updatedCasinoCoins = $casinoCoins + $prize;
-        $user->update(['casino_coins' => $updatedCasinoCoins]);
+        
+        $user->update(['casino_coins' => $updatedCasinoCoins, 'last_used_dg' => Carbon::now()]);
 
-        return redirect()->back();
+        return redirect()->back()->with([
+        'prize' => $prize,
+    ]);
     }
 }
