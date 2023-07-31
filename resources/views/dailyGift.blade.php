7468787cef9655c0adb9799f719ea1c914769da5
WahanH <Wahan2500@hotmail.com>
2023-07-31 22:44:14 +0200

    added pokemon component for won pokemon

--- resources/views/dailyGift.blade.php
+++ resources/views/dailyGift.blade.php
@@ -24,37 +24,37 @@
                     <div class="block">
                         <div class="title"><p>Daily Gift - <a href="/games/ptd/account.php">Go Back</a></p></div>
                         <div>
-                            <p>You have {{($save->money)}} money - <a href="/games/ptd/gameCorner.php">View Prize List</a></p>
+                            <p>You have {{ number_format($save->money, 0, ',', '.') }} money - <a href="/games/ptd/gameCorner.php">View Prize List</a></p>
                             
                             <br>
                             @if (session('prize'))
-                                    <p>Congratulations! You got {{ session('prize') }} Casino Coins!</p>
-                            @else
-                                @if ($user->last_used_dg == now()->toDateString())
-                                    <p>You have already bought your Daily Gift</p>
-                                @else
+                                    <p>Congratulations! You got {{ number_format(session('prize'), 0, ',', '.') }} Casino Coins!</p>
+                            @else  
+                                @if ($dateCheck || $user->last_used_dg == null)
 
-                                    <a href="{{ route('get-gift', ['button' => 1]) }}">Buy a Common Daily Gift for 1000 money</a>
+                                    <p><a href="{{ route('get-gift', ['button' => 1]) }}">Buy a Common Daily Gift for 1.000 money</a></p>
 
-                                    <br>
                                     <br>
 
                                     @if ($save->badges >= 3)
-                                        <p><a href="{{ route('get-gift', ['button' => 2]) }}">Buy an Uncommon Daily Gift for 10000 money</a></p>
+                                        <p><a href="{{ route('get-gift', ['button' => 2]) }}">Buy an Uncommon Daily Gift for 10.000 money</a></p>
                                     @else
                                         <p>You do not have the requirements for the Uncommon Daily Gift in this profile</p>
                                     @endif  
 
                                     <br>
 
-                                    @if ($save->advanced > 10)
-                                        <p><a href="{{ route('get-gift', ['button' => 3]) }}">Buy a Rare Daily Gift for 100000 money</a></p>
+                                    @if ($save->advanced >= 27)
+                                        <p><a href="{{ route('get-gift', ['button' => 3]) }}">Buy a Rare Daily Gift for 100.000 money</a></p>
                                     @else
                                         <p>You do not have the requirements for the Rare Daily Gift in this profile</p>
                                     @endif
+                                @else
+                                    <p>You have already bought your Daily Gift. Come back tomorrow!</p>
                                 @endif
                             @endif
                         </div>
+                    </div>
                 </td>
             </tr>
         </tbody>
