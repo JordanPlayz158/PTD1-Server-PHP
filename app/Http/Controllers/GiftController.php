49edc53fe582d52fb62b17fa09a60e11bb7db2bd
WahanH <Wahan2500@hotmail.com>
2023-07-31 21:48:01 +0200

    made new code for server-based data

--- app/Http/Controllers/GiftController.php
+++ app/Http/Controllers/GiftController.php
@@ -2,61 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\DailyGift;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class GiftController extends Controller
 {
-    public function getGift($button)
+    public function GetGift($button)
     {
-        $user = Auth::user();
-        $save = $user->selectedSave();
-        $randomNumber = mt_rand(0, 1);          
-
-        if(Carbon::parse(Auth::user()->last_used_gc)->addDay()->isBefore(Carbon::now('UTC'))) {
+        if (Carbon::parse(Auth::user()->last_used_gc)->addDay()->isBefore(Carbon::now('UTC'))) {
             return redirect()->back();
         }
 
-        // Define the gift costs for each button
-        $giftCosts = [
-            1 => 1000,
-            2 => 10000,
-            3 => 100000
+        $buttonToGiftIdMapping = [
+            1 => [1, 2], // Map button 1 to gift with id 1 and 2
+            2 => [3, 4], // Map button 2 to gift with id 3 and 4
+            3 => [5, 6], // Map button 3 to gift with id 5 and 6
         ];
 
-        // Define the gift prizes for each button
-        $giftPrizes = [
-            1 => [1000, 10000],
-            2 => [10000, 25000],
-            3 => [50000, 100000],
-        ];
-        // Retrieve the user's / save's info
-        $money = $save->money;
-        $casinoCoins = Auth::user()->casino_coins;
-
-        // Check if the profile has enough money to buy the gift
-        $giftCost = $giftCosts[$button];
-        if ($money < $giftCost) {
-            return redirect()->back()->with('error', 'Not enough coins to buy the gift');
+        if (empty($buttonToGiftIdMapping[$button])) {
+            return redirect()->back();
         }
 
-        // Update the profile's money after buying the daily gift
-        $updatedMoney = $money - $giftCost;
-        $save->update(['money' => $updatedMoney]);
+        $gift = DailyGift::whereIn('id', $buttonToGiftIdMapping[$button])->inRandomOrder()->first();
 
-        // Generate a random number between 0 and 1
-        $prize = $giftPrizes[$button][$randomNumber];
+        if (!$gift) {
+            return redirect()->back();
+        }
 
-        // Update the user's casino coins after buying the daily gift
-        $updatedCasinoCoins = $casinoCoins + $prize;
-        
-        $user->casino_coins = $updatedCasinoCoins;
-        $user->last_used_dg = Carbon::now('UTC');
-        $user->save();
+        if (Auth::user()->selectedSave()->money < $gift->cost) {
+            return redirect()->back();
+        }
+
+        $updatedMoney = Auth::user()->selectedSave()->money - $gift->cost;
+        Auth::user()->selectedSave()->update(['money' => $updatedMoney]);
+
+        $updatedCasinoCoins = Auth::user()->casino_coins + $gift->prize;
+
+        Auth::user()->casino_coins = $updatedCasinoCoins;
+        Auth::user()->last_used_dg = Carbon::now('UTC');
+        Auth::user()->save();
 
         return redirect()->back()->with([
-        'prize' => $prize,
-    ]);
+            'prize' => $gift->prize,
+        ]);
     }
 }
