b70d2b1220ac5143a9164b85c56fc0a54577a19a
WahanH <Wahan2500@hotmail.com>
2023-08-02 19:13:33 +0200

    added a function for the probabilities of prizes

--- app/Http/Controllers/GiftController.php
+++ app/Http/Controllers/GiftController.php
@@ -8,40 +8,73 @@ use Illuminate\Support\Facades\Auth;
 
 class GiftController extends Controller
 {
+    public function selectPrize($button)
+    {
+        // Retrieve all prizes from the database
+        $gifts  = DailyGift::where('button', $button)->get();
+
+        $totalProbability = $gifts->sum('percentage');
+
+        // Normalize probabilities to ensure they add up to 1
+        $normalizedGifts = $gifts->map(function ($gift) use ($totalProbability) {
+            return $gift->percentage / $totalProbability;
+        });
+
+        $randomNumber = mt_rand() / mt_getrandmax();
+
+        $sum = 0;
+
+        foreach ($normalizedGifts as $key => $gift) {
+            $sum += $gift;
+
+            if ($randomNumber < $sum || ($randomNumber == 1 && $sum == 1)) {
+                return $gifts[$key];
+            }
+        }
+    }
+    
+
     public function GetGift($button)
     {
         if (Carbon::parse(Auth::user()->last_used_gc)->addDay()->isBefore(Carbon::now('UTC'))) {
             return redirect()->back();
         }
         
-        // this will only work if the id's in the database are going off by the seeder | 31-7-2023
-        $buttonToGiftIdMapping = [
-            1 => [1, 2], // Map button 1 to gift with id 1 and 2
-            2 => [3, 4], // Map button 2 to gift with id 3 and 4
-            3 => [5, 6], // Map button 3 to gift with id 5 and 6
-        ];
+        $gift  = $this->selectPrize($button);
 
-        $gift = DailyGift::whereIn('id', $buttonToGiftIdMapping[$button])->inRandomOrder()->first();
-        
-        if (Auth::user()->selectedSave()->money < $gift->cost) {
+        if (empty($gift)) {
             return redirect()->back();
         }
 
-        if (empty($buttonToGiftIdMapping[$button])) {
+        if (Auth::user()->selectedSave()->money < $gift->cost) {
             return redirect()->back();
         }
 
         $updatedMoney = Auth::user()->selectedSave()->money - $gift->cost;
         Auth::user()->selectedSave()->update(['money' => $updatedMoney]);
 
-        $updatedCasinoCoins = Auth::user()->casino_coins + $gift->prize;
+        if ($gift->prize <= 20)
+        {
+            $updatedPTDCoins = Auth::user()->ptd_coins + $gift->prize;
+            Auth::user()->ptd_coins = $updatedPTDCoins;
 
-        Auth::user()->casino_coins = $updatedCasinoCoins;
-        Auth::user()->last_used_dg = Carbon::now('UTC');
-        Auth::user()->save();
+            Auth::user()->last_used_dg = Carbon::now('UTC');
+            Auth::user()->save();
 
-        return redirect()->back()->with([
-            'prize' => $gift->prize,
-        ]);
+            return redirect()->back()->with([
+                'prize' => "$gift->prize PTD Coins",
+            ]);
+        }
+        else {
+            $updatedCasinoCoins = Auth::user()->casino_coins + $gift->prize;
+            Auth::user()->casino_coins = $updatedCasinoCoins;
+
+            Auth::user()->last_used_dg = Carbon::now('UTC');
+            Auth::user()->save();
+            
+            return redirect()->back()->with([
+                'prize' => "$gift->prize Casino Coins",
+            ]);
+        }
     }
 }
