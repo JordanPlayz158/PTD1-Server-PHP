73b1a6ef805a3e90ede5c0e9a54a4cc018735150
WahanH <Wahan2500@hotmail.com>
2023-08-02 19:15:50 +0200

    changed table name, added percentages and button

--- database/migrations/2023_07_29_182224_create_daily_gift_table.php
+++ database/migrations/2023_07_29_182224_create_daily_gift_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,10 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('daily_gift', function (Blueprint $table) {
+        Schema::create('dailygift', function (Blueprint $table) {
             $table->id();
+            $table->enum('button', [1, 2 ,3]);
             $table->integer('prize', false, true);
             $table->integer('cost', false, true);
+            $table->decimal('percentage', 5, 5, true);
         });
     }
 
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('daily_gift');
+        Schema::dropIfExists('dailygift');
     }
 };
