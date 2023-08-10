d3d59ff19c5a83c41e2a121f320bfbfc523bbad0
WahanH <Wahan2500@hotmail.com>
2023-08-10 22:27:33 +0200

    changed the table name

--- database/migrations/2023_07_29_182203_create_gamecorner_pokemon_table.php
+++ database/migrations/2023_07_29_182203_create_gamecorner_pokemon_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('gamecorner_pokemon', function (Blueprint $table) {
+        Schema::create('game_corner_pokemon', function (Blueprint $table) {
             $table->id();
             $table->mediumInteger('pNum', false, true);
             $table->string('nickname', 255);
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('gamecorner_pokemon');
+        Schema::dropIfExists('game_corner_pokemon');
     }
 };
