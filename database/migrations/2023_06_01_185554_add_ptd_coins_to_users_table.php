0202ac9436d405d22e1ea73026db9af4191c0da6
WahanH <Wahan2500@hotmail.com>
2023-08-10 22:26:32 +0200

    changed migration file name and snd_coins to ptd_coins

--- database/migrations/2023_06_01_185554_add_ptd_coins_to_users_table.php
+++ database/migrations/2023_06_01_185554_add_ptd_coins_to_users_table.php
@@ -21,6 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropColumns('users', ['snd_coins']);
+        Schema::dropColumns('users', ['ptd_coins']);
     }
 };
