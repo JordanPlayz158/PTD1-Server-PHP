7a6c892761867c5c517a6579ea1128185558000b
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2023-09-12 12:39:35 -0400

    Version 650 support (currently disabled due to potential security issue/risk)

--- app/Http/Controllers/Web/SWFController.php
+++ app/Http/Controllers/Web/SWFController.php
@@ -21,26 +21,27 @@ use Log;
 
 class SWFController extends Controller {
     # Array of ASCII Chars that turns into the AES KEY
-    private static array $aesKeyArray = [99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,118,202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,183,253,147,38,54,63,247,204,52,165,229,241,113,216,49,21,4,199,35,195,24,150,5,154,7,18,128,226,235,39,178,117,9,131,44,26,27,110,90,160,82,59,214,179,41,227,47,132,83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,208,239,170,251,67,77,51,133,69,249,2,127,80,60,159,168,81,163,64,143,146,157,56,245,188,182,218,33,16,255,243,210,205,12,19,236,95,151,68,23,196,167,126,61,100,93,25,115,96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,224,50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,231,200,55,109,141,213,78,169,108,86,244,234,101,122,174,8,186,120,37,46,28,166,180,198,232,221,116,31,75,189,139,138,112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,158,225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,140,161,137,13,191,230,66,104,65,153,45,15,176,84,187,22];
-
+    private static array $aesKeyArray = [197, 158, 28, 139, 240, 118, 85, 109, 129, 5, 36, 226, 16, 167, 50, 50, 197, 158, 28, 139, 240, 118, 85, 109, 129, 5, 36, 226, 16, 167, 50, 50];
 
     private string $aesKey = '';
-    private Encrypter $encryptor;
 
     public function __construct() {
-        /*foreach (SWFController::$aesKeyArray as $keyByte) {
+        foreach (SWFController::$aesKeyArray as $keyByte) {
             $this->aesKey .= chr($keyByte);
         }
-
-        Log::debug('AES Key Length', [strlen($this->aesKey)]);
-        Log::debug('AES Key Length in 8-bit?', [mb_strlen($this->aesKey, '8bit')]);
-
-        $this->encryptor = new Encrypter($this->aesKey, 'aes-256-cbc');*/
     }
 
     public function post(SWFRequest $request): Response {
+        $version = $request->input('ver');
+
         if(app()->isDownForMaintenance()) {
-            return SWFBuilder::new()->setResult(Result::FAILURE())->setReason(Reason::MAINTENANCE())->create();
+            $maintenance = SWFBuilder::new()->setResult(Result::FAILURE())->setReason(Reason::MAINTENANCE())->create();
+
+            if($version == 650) {
+                //return $this->encryptedResponse($maintenance);
+            }
+
+            return $maintenance;
         }
 
         Log::info('Request:', [request()->all()]);
@@ -49,18 +50,19 @@ class SWFController extends Controller {
         $email = $request->input('Email');
         $password = $request->input('Pass');
         $action = $request->input('Action');
-        $version = $request->input('ver');
 
+
+        if($version == 650) {
+            if($action === 'createAccount') {
+                //return $this->encryptedResponse(SWFBuilder::new()->setResult(Result::FAILURE())->setReason(Reason::OLD_VERSION())->create());
+            } else if($action === 'saveAccount') {
+                return SWFBuilder::new()->setResult(Result::FAILURE())->create();
+            }
+        }
 
 
         if($action === 'createAccount') {
-            $createAccountResponse = $this->createAccount($email, $password);
-
-            if($version == 650) {
-                return $this->encryptedResponse($createAccountResponse);
-            }
-
-            return $createAccountResponse;
+            return $this->createAccount($email, $password);
         }
 
         if(!$request->authenticate()) {
@@ -98,8 +100,6 @@ class SWFController extends Controller {
     }
 
     private function createAccount(string $email, string $password): Response {
-        // TODO: Users with invalid emails should not be created
-
         $user = User::firstOrCreate(['email' => $email], ['password' => Hash::make($password)]);
 
         event(new Registered($user));
@@ -445,6 +445,11 @@ class SWFController extends Controller {
     }
 
     private function encryptedResponse(Response $plainResponse) : Response {
-        return response($this->encryptor->encryptString($plainResponse));
+        // Content-Type is probably not needed but figured I'd make it accurate
+        //$rawResponse = ;
+
+        $rootDirectory = $_SERVER['DOCUMENT_ROOT'];
+        // Uncomment when safe way to do this is found (or if exec is safe/can be made safe)
+        //return response(exec("$rootDirectory/../PTD1-AES encrypt lkafd8halkf 256 \"$plainResponse->original\""), 200, ['Content-Type' => 'application/octet-stream']);
     }
 }
