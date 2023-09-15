26a50e17d164bb7da6dbcc82d617dd66f0b6401a
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2023-09-15 15:08:31 -0400

    Fixed nav.html and changed urls for some places on nav.blade.php to make areas more findable

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,14 +5,14 @@
         <ul class="menu">
             <li><a href="/">Blog</a></li>
             <!-- Change to proper check pokemon screen once done -->
-            <li><a href="/games/ptd/createTrade.php">Home</a></li>
+            <li><a href="/games/ptd/account.php">Home</a></li>
             <li><a href="/games/ptd/adoption.php">Pokemon Adoption</a></li>
-            <li><a href="/games/ptd/avatarStore.php">Avatar Store</a></li>
+            <li><a href="/games/ptd/changeAvatar.php">Avatar Store</a></li>
             <li><a href="/games/ptd/dailyGift.php">Daily Gift</a></li>
             <li class="expanded"><a href="/games/ptd/inventory.php">Inventory</a>
                 <ul class="menu">
                     <li><a href="/games/ptd/inventory_items.php">Items</a></li>
-                    <li><a href="/games/ptd/inventory_avatar.php">Avatars</a></li>
+                    <li><a href="/games/ptd/changeAvatar.php">Avatars</a></li>
                 </ul>
             </li>
             <li><a href="/games/ptd/gameCorner.php">Game Corner</a></li>
@@ -32,14 +32,14 @@
                     <li><a href="/games/ptd/latestTrades.php">Latest Trades</a></li>
                 </ul>
             </li>
-            <li class="expanded"><a href="/games/ptd/createTrade.php">Utilities</a>
+            <li class="expanded"><a href="#">Utilities</a>
                 <ul class="menu">
                     <li><a href="/games/ptd/transferTo2.php">Transfer to PTD 2</a></li>
                     <li><a href="/games/ptd/removeHack.php">Remove Hacked Tag</a></li>
                     <li><a href="/games/ptd/elite4fix.php">Elite 4 Black Screen Fix</a></li>
                 </ul>
             </li>
-            <li><a href="/logout">Logout</a></li>
+
         </ul>
     </div>
     <?php
@@ -59,6 +59,7 @@
                             <li><a href="/games/ptd/changeAvatar.php">Change Avatar</a></li>
                             <li><a href="/games/ptd/reset_password_form.php">Change Password</a></li>
                             <li><a href="/apiKeys">API Keys</a></li>
+                            <li><a href="/logout">Logout</a></li>
                         </ul>
                     </a>
                 </li>
